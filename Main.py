397b4b0a61e7456c5b484ec70b1c947d235472c2
dsprad99 <93533488+dsprad99@users.noreply.github.com>
2023-09-07 20:36:49 -0400

    Put "articles" into a paper object

--- Main.py
+++ Main.py
@@ -1,51 +1,53 @@
 import gzip
 
-def printPaper(line):
-    #print(line)
-    
-    #Attributes include
-    #<booktitle>
-    #<year>
-    #<pages>
-    #<title>
-    #<author>
-    #<url>
-    #<crossref>
-    #<ee>
-    #<pages>
+class Paper:
 
+    def __init__(self):
+        self.author = None
+        self.booktitle = None
+        self.year = None
+        self.pages = None
+        self.title = None
+        self.url = None
 
-    if '<title>' in line:
-        title = line.strip().replace('<title>', 'Title: ').replace('</title>', '')
-        print(title)
-    
-    elif '<author>' in line:
-        author = line.replace('<author>', 'Author: ').replace('</author>', '')
-        print( author)
+        
 
+def parse_paper_attributes(file_path, callback):
+    current_paper = None
 
-def listDBLP(file_path, callback):
-    count = 0
-    with gzip.open(file_path, 'rt') as gz_file:
+    with gzip.open(file_path, 'rt', encoding='utf-8') as gz_file:
         for line in gz_file:
-            callback(line)
+            if '<article' in line:
+                current_paper = Paper()
+            elif '</article>' in line and current_paper:
+                callback(current_paper)
+                current_paper = None
+            elif current_paper:
+                if '<author>' in line:
+                    current_paper.author = line.replace('<author>', '').replace('</author>', '').strip()
+                elif '<booktitle>' in line:
+                    current_paper.booktitle = line.replace('<booktitle>', '').replace('</booktitle>', '').strip()
+                elif '<year>' in line:
+                    current_paper.year = line.replace('<year>', '').replace('</year>', '').strip()
+                elif '<pages>' in line:
+                    current_paper.pages = line.replace('<pages>', '').replace('</pages>', '').strip()
+                elif '<title>' in line:
+                    current_paper.title = line.replace('<title>', '').replace('</title>', '').strip()
+                elif '<url>' in line:
+                    current_paper.url = line.replace('<url>', '').replace('</url>', '').strip()
+
+
+def print_paper(paper):
+    print("Author:", paper.author)
+    print("Title:", paper.title)
+    print("Year:", paper.year)
+    print("Pages:", paper.pages)
+    print("Booktitle:", paper.booktitle)
+    print("URL:", paper.url)
+    print()
 
 file_path = 'dblp.xml.gz'
-listDBLP(file_path, printPaper)
-
-
-
-
-
-
-
-
-#listDBLP(“filename”, function):
-#printPaper(“filename”)
-
-#Def printpaper(paper):
-#print(paper.title)
-#print(paper.author)
+parse_paper_attributes(file_path, print_paper)
 
 
 
