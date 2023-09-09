16f147ac7c6019e6c98dbc652805837aaf88e47b
dsprad99 <93533488+dsprad99@users.noreply.github.com>
2023-09-09 18:08:36 -0400

    Parsed both DBLP and MAG data

--- Main.py
+++ Main.py
@@ -1,14 +1,17 @@
 import gzip
+import sys
 
 class Paper:
 
     def __init__(self):
+        self.paper_id = None
         self.author = None
         self.booktitle = None
         self.year = None
         self.pages = None
         self.title = None
         self.url = None
+        self.published_through = None
 
         
 
@@ -16,11 +19,16 @@ def parse_paper_attributes(file_path, callback):
     current_paper = None
 
     with gzip.open(file_path, 'rt', encoding='utf-8') as gz_file:
+        count = 0
         for line in gz_file:
             if '<article' in line:
                 current_paper = Paper()
             elif '</article>' in line and current_paper:
-                callback(current_paper)
+
+                if count<10:
+                    callback(current_paper)
+                    count += 1
+
                 current_paper = None
             elif current_paper:
                 if '<author>' in line:
@@ -37,17 +45,52 @@ def parse_paper_attributes(file_path, callback):
                     current_paper.url = line.replace('<url>', '').replace('</url>', '').strip()
 
 
+def parse_MAG_file(file_path, callback):
+    # Replace 'Papers.txt.gz' with the actual file path.
+    count = 0
+    file_path = 'Papers.txt.gz'
+    with gzip.open(file_path, 'rt', encoding='utf-8') as file:
+        for line in file:
+            fields = line.strip().split('\t')
+            current_paper = Paper()
+            paper_identification, paper_title = fields[0], fields[4]
+
+            current_paper.paper_id = paper_identification
+            current_paper.title = paper_title
+            if(count<10):
+                callback(current_paper)
+                count+=1
+
+
+
 def print_paper(paper):
     print("Author:", paper.author)
     print("Title:", paper.title)
+    print("Paper ID", paper.paper_id)
     print("Year:", paper.year)
     print("Pages:", paper.pages)
     print("Booktitle:", paper.booktitle)
     print("URL:", paper.url)
     print()
 
-file_path = 'dblp.xml.gz'
-parse_paper_attributes(file_path, print_paper)
+file_path_dblp = 'dblp.xml.gz'
+parse_paper_attributes(file_path_dblp, print_paper)
+
+file_path_MAG = 'Papers.txt.gz'
+parse_MAG_file(file_path_MAG, print_paper)
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
