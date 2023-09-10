fe032a5e27ac9560672f30bc838a00e49ac1a40a
dsprad99 <93533488+dsprad99@users.noreply.github.com>
2023-09-10 15:10:07 -0400

    Counts number of Paper titles and characters

--- Main.py
+++ Main.py
@@ -6,7 +6,6 @@ class Paper:
     def __init__(self):
         self.paper_id = None
         self.author = None
-        self.booktitle = None
         self.year = None
         self.pages = None
         self.title = None
@@ -14,10 +13,14 @@ class Paper:
         self.published_through = None
 
         
+title_character_counter = 0
+number_of_titles = 0
+
 
 def parse_paper_attributes(file_path, callback):
     current_paper = None
-
+    global title_character_counter
+    global number_of_titles
     with gzip.open(file_path, 'rt', encoding='utf-8') as gz_file:
         count = 0
         for line in gz_file:
@@ -33,20 +36,22 @@ def parse_paper_attributes(file_path, callback):
             elif current_paper:
                 if '<author>' in line:
                     current_paper.author = line.replace('<author>', '').replace('</author>', '').strip()
-                elif '<booktitle>' in line:
-                    current_paper.booktitle = line.replace('<booktitle>', '').replace('</booktitle>', '').strip()
                 elif '<year>' in line:
                     current_paper.year = line.replace('<year>', '').replace('</year>', '').strip()
                 elif '<pages>' in line:
                     current_paper.pages = line.replace('<pages>', '').replace('</pages>', '').strip()
                 elif '<title>' in line:
                     current_paper.title = line.replace('<title>', '').replace('</title>', '').strip()
+                    title_character_counter += len(current_paper.title)
+                    number_of_titles += 1
                 elif '<url>' in line:
                     current_paper.url = line.replace('<url>', '').replace('</url>', '').strip()
 
 
 def parse_MAG_file(file_path, callback):
     # Replace 'Papers.txt.gz' with the actual file path.
+    global title_character_counter
+    global number_of_titles
     count = 0
     file_path = 'Papers.txt.gz'
     with gzip.open(file_path, 'rt', encoding='utf-8') as file:
@@ -54,6 +59,9 @@ def parse_MAG_file(file_path, callback):
             fields = line.strip().split('\t')
             current_paper = Paper()
             paper_identification, paper_title = fields[0], fields[4]
+
+            title_character_counter += len(fields[4])
+            number_of_titles += 1
 
             current_paper.paper_id = paper_identification
             current_paper.title = paper_title
@@ -69,7 +77,6 @@ def print_paper(paper):
     print("Paper ID", paper.paper_id)
     print("Year:", paper.year)
     print("Pages:", paper.pages)
-    print("Booktitle:", paper.booktitle)
     print("URL:", paper.url)
     print()
 
@@ -78,6 +85,9 @@ parse_paper_attributes(file_path_dblp, print_paper)
 
 file_path_MAG = 'Papers.txt.gz'
 parse_MAG_file(file_path_MAG, print_paper)
+
+print("Total characters parsed for all Paper titles ",title_character_counter)
+print("Total number of paper titles ",number_of_titles)
 
 
 
