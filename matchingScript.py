ee424b0e238ac9b5630f8df3556c444d04659669
Davis Spradling <93533488+dsprad99@users.noreply.github.com>
2024-01-25 11:28:26 -0800

    updated parallelization script

--- matchingScript.py
+++ matchingScript.py
@@ -1,35 +1,39 @@
+import argparse
 from time_trial import average_histogram, perform_trials
 from Parse import parse_MAG_file
-from matchingProgram import matching_process,build_dblp_hash_table,csv_writer,successful_candidates,total_candidates
+from matchingProgram import matching_process, build_dblp_hash_table, csv_writer, successful_candidates, total_candidates
 
+parser = argparse.ArgumentParser(description='Your script description')
+parser.add_argument('--start', type=int, help='Start value for queries')
+parser.add_argument('--end', type=int, help='End value for queries')
+parser.add_argument('--fileName', type=str, help='CSV File Name')
+
+args = parser.parse_args()
 
 k_value = 3
-paper_limit = 100000000000000
+paper_limit = 6649168
 repeating_mers_remove = 0
 num_removed_kmers = 0
 levenshtein_candidates = 10
 results = []
 
-
 dblp_mer_hash, paper_details, hashmap_build_time = build_dblp_hash_table(k_value, paper_limit, repeating_mers_remove)
 
 queries = [
-        (0, 5),
-        (6, 10),
-        (11, 15),
-        (16, 20),
-        (21, 25)
-    ]
+    (args.start, args.end)
+]
 
-callbacks = [lambda currentPaper: results.extend(matching_process(k_value, dblp_mer_hash, num_removed_kmers, levenshtein_candidates, paper_details,hashmap_build_time,currentPaper.title))]
+callbacks = [lambda currentPaper: results.extend(matching_process(k_value, dblp_mer_hash, num_removed_kmers,
+                                                                  levenshtein_candidates, paper_details,
+                                                                  hashmap_build_time, currentPaper.title))]
 
-index = 0
-for (start,end) in queries:
+for (start, end) in queries:
     parse_MAG_file(callbacks, start, end)
-    
 
-fileName = "matchingScriptTest.csv"
-csv_writer(results,fileName)
+newFileName = f"{args.fileName}_{args.start}_{args.end}.csv"
+
+csv_writer(results, newFileName)
+
 
 
     
