956eddc8f9ae31a32ab71e3f434cfd565781d9e0
Davis Spradling <93533488+dsprad99@users.noreply.github.com>
2024-01-30 09:38:07 -0500

    update processing matchingScript.py

--- matchingScript.py
+++ matchingScript.py
@@ -11,24 +11,24 @@ parser.add_argument('--fileName', type=str, help='CSV File Name')
 args = parser.parse_args()
 
 k_value = 5
+#Note that DBLP has 6649168 papers set this as the max paper_limit
 paper_limit = 6649168
 repeating_mers_remove = 10
 num_removed_kmers = 1000
 levenshtein_candidates = 10
 results = []
+start = args.start
+end = args.end
 
 dblp_mer_hash, paper_details, hashmap_build_time = build_dblp_hash_table(k_value, paper_limit, repeating_mers_remove)
 
-queries = [
-    (args.start, args.end)
-]
+print(args.start,args.end)
 
 callbacks = [lambda currentPaper: results.extend(matching_process(k_value, dblp_mer_hash, num_removed_kmers,
                                                                   levenshtein_candidates, paper_details,
                                                                   hashmap_build_time, currentPaper.title))]
 
-for (start, end) in queries:
-    parse_MAG_file(callbacks, start, end)
+parse_MAG_file(callbacks, start, end)
 
 newFileName = f"{args.fileName}_{args.start}_{args.end}.csv"
 
