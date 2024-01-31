2bcdd824b44209ab8c1085707176ec0006cdb5cb
Davis Spradling <93533488+dsprad99@users.noreply.github.com>
2024-01-31 11:19:03 -0500

    update matchingScript.py

--- matchingScript.py
+++ matchingScript.py
@@ -4,15 +4,16 @@ from Parse import parse_MAG_file
 from matchingProgram import matching_process, build_dblp_hash_table, csv_writer, successful_candidates, total_candidates
 
 parser = argparse.ArgumentParser(description='Your script description')
+parser.add_argument('--paperLimit', type=int, help='Paper limit for queries')
 parser.add_argument('--start', type=int, help='Start value for queries')
 parser.add_argument('--end', type=int, help='End value for queries')
 parser.add_argument('--fileName', type=str, help='CSV File Name')
 
 args = parser.parse_args()
 
-k_value = 5
+k_value = 8
 #Note that DBLP has 6649168 papers set this as the max paper_limit
-paper_limit = 6649168
+paper_limit = args.paperLimit
 repeating_mers_remove = 10
 num_removed_kmers = 1000
 levenshtein_candidates = 10
